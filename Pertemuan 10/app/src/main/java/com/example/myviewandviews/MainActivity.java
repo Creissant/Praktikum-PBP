8aa24e942ad249ca6e2e5f00b3d90412a3f5c32e
Fahrel <fahrel.gelora@gmail.com>
2022-11-18 21:54:36 +0700

    adding contact button

--- Pertemuan 10/app/src/main/java/com/example/myviewandviews/MainActivity.java
+++ Pertemuan 10/app/src/main/java/com/example/myviewandviews/MainActivity.java
@@ -21,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         buy.setOnClickListener(v -> {
             startActivity(new Intent(this, BuyActivity.class));
         });
+
+        Button contact = findViewById(R.id.contact);
+        contact.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(android.net.Uri.parse("tel:+6281270360350"));
+            startActivity(intent);
+        });
     }
 }
